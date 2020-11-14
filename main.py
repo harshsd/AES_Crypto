79380c9f1207b37ea52f872147986e4ee89940dc
Harsh Deshpande <harshdeshpande@Apples-MacBook-Pro.local>
2020-11-14 15:18:01 +0530

    lolmax

--- main.py
+++ main.py
@@ -12,15 +12,15 @@ num_rounds = 4
 
 key_stream = expand_key(Key,num_rounds,S_table)
 
-print(key_stream)
+print("Key stram is: ",key_stream)
 
 input_stream = [np.random.randint(128) for x in range(16)]  #Input stream with 8 bits(1 byte) at a time
-print(input_stream)
+print("Input Stream is: ",input_stream)
 
 
 encrypt_stream = encrypt_stream(input_stream,S_table,num_rounds,key_stream)
 
-decrypt_stream = decrypt_stream(input_stream,inv_S_table,num_rounds,key_stream)
+decrypt_stream = decrypt_stream(encrypt_stream,inv_S_table,num_rounds,key_stream)
 
-print(input_stream)
-print(decrypt_stream)
+print("Input Stream is: ",input_stream)
+print("Decrpyted Stream is: ",decrypt_stream)
